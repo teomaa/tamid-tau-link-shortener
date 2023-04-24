0875d532f7d9902e07eca1ac13d6916a75456d23
Teo Maayan <maayanteo@gmail.com>
2023-04-25 01:40:35 +0300

    Code cleanup, some documentation
    Signed-off-by: Teo Maayan <maayanteo@gmail.com>

--- frontend/script.js
+++ frontend/script.js
@@ -1,13 +1,30 @@
 $(function () {
+    // Redirect
+    console.log(location.href)
+    var visited_url = location.href
+    var short_url = visited_url.split('/')
+    short_url = short_url[short_url.length - 1]
+
+    console.log(short_url)
+    if (short_url != null && short_url.length === 6) { // Make sure short URL is in the correct format
+        $.ajax({
+            url: "https://tamid-tau-link-shortener-backend-lz7muikcfa-zf.a.run.app/read?short_url=" + short_url,
+            contentType: "application/json",
+            data: "",
+            type: 'GET',
+            success: function (response) {
+                console.log("Test")
+                console.log(response)
+                location.href = "https://" + response;
+
+            }
+        });
+    }
+
     $('.alert').hide()
     $('#main_button').click(function () {
         var url = $('#url_text_field').val()
-        // $('#in_form').val()
-        // var data = {}  // object to hold the user input data
-        // store user data in a data["pycatj_data"]
 
-        // todo: add root input element
-        // data["root"] = "POST"
         console.log(url)
 
         $.ajax({
@@ -19,37 +36,10 @@ $(function () {
                 // $('#out_form').val(response.data)
                 console.log("Test")
                 console.log(response)
+                $('#short_url_clickable').text("tamid.org.il/" + response)
+                $('#short_url_clickable').attr("href", "https://tamid.org.il/" + response)
                 $('.alert').show()
             }
         });
     });
-
-    $('#redirect_button').click(function () {
-        var url = $('#shorturl_text_field').val()
-        // $('#in_form').val()
-        // var data = {}  // object to hold the user input data
-        // store user data in a data["pycatj_data"]
-
-        // todo: add root input element
-        // data["root"] = "POST"
-        console.log(url)
-
-        $.ajax({
-            url: "https://tamid-tau-link-shortener-backend-lz7muikcfa-zf.a.run.app/read?short_url=" + url,
-            contentType: "application/json",
-            data: "",
-            type: 'GET',
-            success: function (response) {
-                // $('#out_form').val(response.data)
-                console.log("Test")
-                console.log(response)
-                location.href = "https://" + response;
-
-            }
-        });
-    });
-});
-
-$(function () {
-
 });
