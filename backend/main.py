ef22368274d924d79f16fbfe069bd33a587a95c3
Teo Maayan <maayanteo@gmail.com>
2023-04-25 04:08:28 +0300

    Documentation
    Signed-off-by: Teo Maayan <maayanteo@gmail.com>

--- backend/main.py
+++ backend/main.py
@@ -9,19 +9,25 @@ from flask_cors import CORS, cross_origin
 import random
 import string
 
+# Create the Flask Web server object
 app = Flask(__name__)
 
+# Set up CORS in order to be able to send requests from external servers
 cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
 
+# Connect to database
 cred = credentials.Certificate("firebase_cert.json")
 firebase_admin.initialize_app(cred)
 db = firestore.client()
 url_database = db.collection('urls')
 
+# All ASCII (ie english) lowercase letters, used for URL generation
 ascii_letters = string.ascii_lowercase
 
 
+# Endpoint to get long url if you have a short one
+# Gets data from connected DB
 @app.route('/read', methods=['GET'])
 @cross_origin()
 def read_data():
@@ -33,6 +39,7 @@ def read_data():
         return f"An Error Occurred: {e}"
 
 
+# Method to save a new url
 @app.route('/save', methods=['POST'])
 @cross_origin()
 def save_data():
@@ -46,17 +53,22 @@ def save_data():
         return f"An Error Occurred: {traceback.format_exc()}"
 
 
+# Method to add a new url without being logged in
 @app.route('/save-anon', methods=['POST'])
 @cross_origin()
 def save_anonymous():
 # Requires JSON encoded long url as follows:
 # { "long_url":"<LONG URL>" }
+
     try:
         json = request.json
+
+        # Check if url already exists
         existing_entry = url_database.where(u'long_url', u'==', json['long_url']).get()
         if len(existing_entry) > 0:
             return existing_entry[0].id, 200
 
+        # Save urls consistently (w/o https:// prefix)
         if 'https://' in json['long_url']:
             json['long_url'] = json['long_url'].replace('https://', '')
         elif 'http://' in json['long_url']:
@@ -67,12 +79,15 @@ def save_anonymous():
         json['short_url'] = short_url
         json['views'] = 0
         json['user'] = "anon"
+
+        # Save document to database
         url_database.document(json['id']).set(json)
         return short_url, 200
     except Exception as e:
         return f"An Error Occurred: {traceback.format_exc()}"
 
 
+# Entrypoint
 port = int(os.environ.get('PORT', 8080))
 if __name__ == '__main__':
     app.run(threaded=True, host='0.0.0.0', port=port)
